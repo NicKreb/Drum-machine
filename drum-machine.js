d92e585fa86d94b2865a33bff02b37611ed61e3e
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-07-06 09:23:52 -0500

    Update drum-machine.js

--- drum-machine.js
+++ drum-machine.js
@@ -1,1 +1,76 @@
 
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
+import React from 'https://esm.sh/react@18.2.0'
+
+const soundGroup = [
+  {
+    keycode: 81,
+    key: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keycode: 87,
+    key: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keycode: 69,
+    key: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keycode: 65,
+    key: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keycode: 83,
+    key: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keycode: 68,
+    key: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keycode: 90,
+    key: 'Z',
+    id: 'Kick-n-Hat',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keycode: 88,
+    key: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keycode: 67,
+    key: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+const Keyboard = () => {
+  return soundGroup.map(sound => {
+    return <button>
+      <audio src={sound.url} />
+    </button>
+  })
+}
+
+const App = () => {
+  return <div id="drum-machine">
+    <Keyboard />
+  </div>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
