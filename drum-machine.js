ceafdcb99195ebff948b3ff3db110079dfce75ef
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-07-10 21:24:40 -0500

    Update drum-machine.js

--- drum-machine.js
+++ drum-machine.js
@@ -1,76 +1,152 @@
-
-import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
-import React from 'https://esm.sh/react@18.2.0'
-
-const soundGroup = [
-  {
-    keycode: 81,
-    key: 'Q',
-    id: 'Heater-1',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-  },
-  {
-    keycode: 87,
-    key: 'W',
-    id: 'Heater-2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-  },
-  {
-    keycode: 69,
-    key: 'E',
-    id: 'Heater-3',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-  },
-  {
-    keycode: 65,
-    key: 'A',
-    id: 'Heater-4',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-  },
-  {
-    keycode: 83,
-    key: 'S',
-    id: 'Clap',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-  },
-  {
-    keycode: 68,
-    key: 'D',
-    id: 'Open-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-  },
-  {
-    keycode: 90,
-    key: 'Z',
-    id: 'Kick-n-Hat',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-  },
-  {
-    keycode: 88,
-    key: 'X',
-    id: 'Kick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-  },
-  {
-    keycode: 67,
-    key: 'C',
-    id: 'Closed-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-  }
+const audioClips = [
+    {
+        keyCode: 81,
+        keyTrigger: "Q",
+        id: "Heater-1",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    }, {
+        keyCode: 87,
+        keyTrigger: "W",
+        id: "Heater-2",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    }, {
+        keyCode: 69,
+        keyTrigger: "E",
+        id: "Heater-3",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    }, {
+        keyCode: 65,
+        keyTrigger: "A",
+        id: "Heater-4",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    }, {
+        keyCode: 83,
+        keyTrigger: "S",
+        id: "Clap",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    }, {
+        keyCode: 68,
+        keyTrigger: "D",
+        id: "Open-HH",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    }, {
+        keyCode: 90,
+        keyTrigger: "Z",
+        id: "Kick-n'-Hat",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    }, {
+        keyCode: 88,
+        keyTrigger: "X",
+        id: "Kick",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    }, {
+        keyCode: 67,
+        keyTrigger: "C",
+        id: "Closed-HH",
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    }
 ];
 
-const Keyboard = () => {
-  return soundGroup.map(sound => {
-    return <button>
-      <audio src={sound.url} />
-    </button>
-  })
+function App() {
+    const [volume, setVolume] = React.useState(1);
+    const [recording, setRecording] = React.useState("");
+    const [speed, setSpeed] = React.useState(0.5);
+
+    const playRecording = () => {
+        let index = 0;
+        let recordArray = recording.split(" ");
+            const interval = setInterval(() => {
+                const audioTag = document.getElementById(recordArray[index]);
+        audioTag.volume = volume;
+        audioTag.currentTime = 0;
+        audioTag.play();
+        index++; 
+        }, speed * 600);
+        setTimeout(() => clearInterval(interval), 600 * speed * recordArray.length - 1);
+    };
+    return (
+        <div id="display" className="bg-info min-vh-100 text-white" >
+            <div className="text-center">
+                <h2>Drum Machine</h2>
+                {audioClips.map((clip) => (
+                    <Pad 
+                    key={clip.id} 
+                    clip={clip} 
+                    volume={volume} 
+                    setRecording={setRecording} 
+                    />
+                ))}
+                <br />
+                <h4>Volume</h4>
+                <input 
+                    type="range" 
+                    step="0.01"
+                    onChange={(e) => setVolume(e.target.value)} 
+                    value={volume}
+                    max="1"
+                    min="0"
+                    className="w-50"
+                />
+                <h3>{recording}</h3>
+                {recording && (
+                    <>
+                    <button onClick={playRecording} className="btn btn-success">
+                        play
+                    </button>
+                    <button onClick={() => setRecording("")} className="btn btn-danger">
+                        clear
+                    </button>
+                    <br />
+                    <h4>Speed</h4>
+                    <input 
+                    type="range" 
+                    step="0.01"
+                    onChange={(e) => setSpeed(e.target.value)} 
+                    value={speed}
+                    max="1.2"
+                    min="0.1"
+                    className="w-50"
+                />
+
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
-const App = () => {
-  return <div id="drum-machine">
-    <Keyboard />
-  </div>
+function Pad({ clip, volume, setRecording }) {
+    const [active, setActive] = React.useState(false);
+
+    React.useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress)   
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        }
+    }, [])
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === clip.keyCode) {
+            playSound();
+        }
+    }
+
+    const playSound = () => {
+        const audioTag = document.getElementById(clip.keyTrigger);
+        setActive(true);
+        setTimeout(() => setActive(false), 200);
+        audioTag.volume = volume;
+        audioTag.currentTime = 0;
+        audioTag.play();
+        setRecording(prev => prev + clip.keyTrigger + " ");
+    };
+
+    return (
+        <div onClick={playSound} id={clip.id} className={`drum-pad btn btn-secondary p-4 m-3 ${active && "btn-warning"}`}>
+            <audio className="clip" id={clip.keyTrigger} src={clip.url} />
+            {clip.keyTrigger}
+        </div>
+    );
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("drum-machine"));
